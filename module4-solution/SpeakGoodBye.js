3b980c0a74e17dfb9b54052841d2e9dcf9337402
niharpandya <niharpandya97@gmail.com>
2016-08-05 22:52:19 -0400

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -1,31 +1,31 @@
-// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+// NOTE! The steps in this file are basically identical to the ones you
+// performed in the SpeakHello.js file.
+
+// STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
 
 
-// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
+// STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-// var helloSpeaker =
+// var byeSpeaker =
 
-// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-(function (window) {
-	var helloSpeaker = {}
-	helloSpeaker.speakWord = "Hello";
+// DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+(function(window) {
+	var byeSpeaker = {}
+	byeSpeaker.speakWord = "Good Bye";
 
-
-// STEP 4: Rewrite the 'speak' function such that it is attached to the
-// helloSpeaker object instead of being a standalone function.
+// STEP 8: Rewrite the 'speak' function such that it is attached to the
+// byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-helloSpeaker.speak = (function (name) {
-  console.log(helloSpeaker.speakWord + " " + name);
+byeSpeaker.speak = (function(name) {
+  console.log(byeSpeaker.speakWord + " " + name);
 })
-window.helloSpeaker = helloSpeaker;
+window.byeSpeaker = byeSpeaker;
 })(window);
 
 
-// STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
-// 'helloSpeaker' on the global scope as well.
-// See Lecture 52, part 2
-// (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-// xxxx.xxxx = helloSpeaker;
 
+// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+// 'byeSpeaker' on the global scope as well.
+// xxxx.xxxx = byeSpeaker;
